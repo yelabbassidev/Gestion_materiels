1f617b6ef1b25a1b49a7d5f565e3ab00874211b1
Youssef elabbassi <32640514+yelabbassidev@users.noreply.github.com>
2021-01-22 16:30:08 +0100

    installationDebugbar

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
         Intervention\Image\ImageServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -230,6 +232,7 @@ return [
 
         'Image' => Intervention\Image\Facades\Image::class,
         Spatie\Permission\PermissionServiceProvider::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
 
     ],
